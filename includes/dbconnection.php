db883432dc5c88b4f70aaf89c0ce8c23c5554467
Mohindran01 <61914497+Mohindran01@users.noreply.github.com>
2021-04-28 12:24:20 +0530

    Update dbconnection.php

--- includes/dbconnection.php
+++ includes/dbconnection.php
@@ -1,7 +1,13 @@
 <?php
-$con=mysqli_connect("localhost", "root", "", "cvmsdb");
+//$con=mysqli_connect("localhost", "root", "", "cvmsdb");
+//if(mysqli_connect_errno()){
+//echo "Connection Fail".mysqli_connect_error();
+//}
+
+$con=mysqli_connect("remotemysql.com", "i0Df3QUhZo", "SmLVjTLhRu", "i0Df3QUhZo");
 if(mysqli_connect_errno()){
 echo "Connection Fail".mysqli_connect_error();
 }
+
 
   ?>
