ec8d83d751295eccfb808e7e62505b6177772fd6
MOHINDRAN DK <s.r.mohindran@gmail.com>
2021-04-28 11:38:57 +0530

    first commit

---
+++ includes/sidebar.php
@@ -0,0 +1,40 @@
+<aside class="menu-sidebar d-none d-lg-block">
+            <div class="logo">
+        
+                   <h4>Visitor Management System</h4>
+            </div>
+            <div class="menu-sidebar__content js-scrollbar1">
+                <nav class="navbar-sidebar">
+                    <ul class="list-unstyled navbar__list">
+                        <li>
+                            <a href="dashboard.php"  style="color: blue">
+                                <i class="fas fa-tachometer-alt"></i>Dashboard</a>
+                        </li>
+
+     <li>
+                            <a href="visitors-form.php"  style="color: blue">
+                                <i class="fa fa-user"></i>New Visitor</a>
+                        </li>
+   <li>
+                            <a href="manage-newvisitors.php"  style="color: blue">
+                                <i class="fa fa-users"></i>Manage Visitors</a>
+                        </li>
+	<li>
+                            <a href="maximum_visitors.php"  style="color: blue">
+                                <i class="fa fa-users"></i>Maximum Visitors</a>
+                        </li>
+	<li>
+                            <a href="maximum_peopl.php"  style="color: blue">
+                                <i class="fa fa-users"></i>People inside Office</a>
+                        </li>
+
+                        
+                      <li>
+                            <a href="bwdates-reports.php"  style="color: blue">
+                                <i class="fas fa-copy"></i>Vistors Dates</a>
+                        </li>  
+                       
+                    </ul>
+                </nav>
+            </div>
+        </aside>
