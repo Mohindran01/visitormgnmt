6ad9587d0fada9f17096465f1c9c148c7ab014d6
Mohindran01 <61914497+Mohindran01@users.noreply.github.com>
2021-04-28 11:42:37 +0530

    Add files via upload

---
+++ cvmsdb.sql
@@ -0,0 +1,112 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 28, 2021 at 08:12 AM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.4.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `cvmsdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbladmin`
+--
+
+CREATE TABLE `tbladmin` (
+  `ID` int(5) NOT NULL,
+  `AdminName` varchar(45) DEFAULT NULL,
+  `UserName` char(45) DEFAULT NULL,
+  `MobileNumber` bigint(11) DEFAULT NULL,
+  `Email` varchar(120) DEFAULT NULL,
+  `Password` varchar(120) DEFAULT NULL,
+  `AdminRegdate` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbladmin`
+--
+
+INSERT INTO `tbladmin` (`ID`, `AdminName`, `UserName`, `MobileNumber`, `Email`, `Password`, `AdminRegdate`) VALUES
+(1, 'Admin user', 'admin', 8596131454, 'admin@gmail.com', 'f925916e2754e5e03f75dd58a5733251', '2021-04-26 06:26:14');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblvisitor`
+--
+
+CREATE TABLE `tblvisitor` (
+  `ID` int(5) NOT NULL,
+  `FullName` varchar(120) DEFAULT NULL,
+  `Email` varchar(120) DEFAULT NULL,
+  `MobileNumber` bigint(11) DEFAULT NULL,
+  `Address` varchar(250) DEFAULT NULL,
+  `WhomtoMeet` varchar(120) DEFAULT NULL,
+  `Deptartment` varchar(120) DEFAULT NULL,
+  `ReasontoMeet` varchar(120) DEFAULT NULL,
+  `EnterDate` timestamp NULL DEFAULT current_timestamp(),
+  `remark` varchar(255) DEFAULT NULL,
+  `outtime` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tblvisitor`
+--
+
+INSERT INTO `tblvisitor` (`ID`, `FullName`, `Email`, `MobileNumber`, `Address`, `WhomtoMeet`, `Deptartment`, `ReasontoMeet`, `EnterDate`, `remark`, `outtime`) VALUES
+(1, 'Akash', 'akash@gmail.com', 2147483647, 'Flat No-210, Kamala Nagar Market New Delhi', 'Mr. Birijesh', 'Hr.Department', 'Interview', '2019-03-31 18:30:00', 'done', '2019-04-23 16:03:57'),
+(14, 'mohi', 's.r.mohindran@gmail.com', 11, 'coimbatore', 'noone', 'hr', 'internship', '2021-04-26 16:40:24', 'done', '2021-04-26 16:41:50'),
+(15, 'Mohindran S.R', '1833027mdcs@cit.edu.in', 9944600417, 'coimbatore', 'noone', 'hr', 'internship', '2021-04-26 16:43:08', NULL, NULL),
+(16, 'mohi', 's.r.mohi@gmail.com', 9944607485, 'coimbatore', 'noone', 'hr', 'internship', '2021-04-27 18:28:18', NULL, NULL);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbladmin`
+--
+ALTER TABLE `tbladmin`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `tblvisitor`
+--
+ALTER TABLE `tblvisitor`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbladmin`
+--
+ALTER TABLE `tbladmin`
+  MODIFY `ID` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tblvisitor`
+--
+ALTER TABLE `tblvisitor`
+  MODIFY `ID` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
